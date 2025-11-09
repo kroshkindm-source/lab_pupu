23e20387dd7915fcdad87743e731c0958de65719
Крошкин Дмитрий <kroshkindm@list.ru>
2025-11-09 21:49:05 +0300

    f

--- src/horhe cola and chips.py
+++ src/horhe cola and chips.py
@@ -1,9 +1,23 @@
-print('у тебя есть девушка?')
-a=input()
-if a=='да':
-    print('покупай колу и чипсы')
-elif a=='нет':
-    print('покупай колу и чипсы')
-else:
-    print('еблан?')
-print('a')
+import sqlite3
+
+conn = sqlite3.connect('kitov_A.db')
+cursor = conn.cursor()
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS zaselenie(
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+name TEXT,
+flat INTEGER,
+passport_num TEXT,
+inst TEXT,
+contacts TEXT,
+obshaga_num INTEGER,
+narush TEXT)''')
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS viselenie(
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+id_vis INTEGER FOREIGNKEY REFERENCES zaselenie(id),
+sost TEXT,
+ready BOOLEAN)''')
+conn.commit()
+conn.close()
+
